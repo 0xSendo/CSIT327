c1d9a0586f545b366d2f66e8402e10ca35d62823
0xSendo <142055962+0xSendo@users.noreply.github.com>
2024-12-02 20:00:35 +0800

    Update urls.py

--- Academate/IM2/IM2/urls.py
+++ Academate/IM2/IM2/urls.py
@@ -1,23 +1,8 @@
-"""
-URL configuration for IM2 project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from academate import views
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView 
 
 urlpatterns = [
     # Admin Panel
@@ -25,8 +10,7 @@ urlpatterns = [
     
     # Auth-related Routes
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='landing'), name='logout'),
-    path('logout/', views.custom_logout_view, name='logout'),
+    path('logout/', views.custom_logout_view, name='logout'),  # Custom logout view
     path('register/', views.render_register, name='register'),
     
     # Landing Page
@@ -47,4 +31,9 @@ urlpatterns = [
     
     # Navigation Route
     path('navigation/', views.render_navigation, name='navigation'),
+    
+    # Profile Routes
+    path('profile/', views.render_profile, name='profile'),
+    path('edit-profile/', views.edit_profile, name='edit_profile'),
+    path('update-profile/', views.update_profile, name='update_profile'),  # To handle form submission from edit profile page
 ]
