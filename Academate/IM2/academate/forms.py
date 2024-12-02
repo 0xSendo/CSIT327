d7e9d7e8a5e08b24462332795107456bacdcd330
0xSendo <142055962+0xSendo@users.noreply.github.com>
2024-12-02 19:59:32 +0800

    Update forms.py

--- Academate/IM2/academate/forms.py
+++ Academate/IM2/academate/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.hashers import make_password
 from .models import User, Assignment
 from .models import Journal
+from django.contrib.auth.models import User
+
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(
         widget=forms.PasswordInput,
@@ -52,3 +54,12 @@ class JournalForm(forms.ModelForm):
     class Meta:
         model = Journal
         fields = ['title', 'content']
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password'].widget = forms.PasswordInput() 
